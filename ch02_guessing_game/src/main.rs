4411e70e606938c0ba587164b2192a29733993ca
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2024-03-14 09:07:26 +0700

    ch02: using match for Result enum

--- ch02_guessing_game/src/main.rs
+++ ch02_guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
@@ -18,10 +18,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess
+        let guess: u32 = match guess
             .trim()
             .parse()
-            .expect("Please type a number!");
+            // .expect("Please type a number!");
+            {
+                Ok(num) => num,
+                Err(_) => continue
+            };
 
         println!("You guessed: {guess}");
 
